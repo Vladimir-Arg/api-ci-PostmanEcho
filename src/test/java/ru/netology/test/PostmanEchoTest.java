f0ffe8c25723ef92358e06ce179e21a65306135d
Vladimir Arger <8745@rambler.ru>
2023-09-12 09:04:48 +0400

    fix test

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"))
+                .body("data", equalTo("Hi"))
         ;
     }
 }
